dfeeade32b13a248161b949097d5a709f3275189
sapt3 <saptarshi.ironmaiden@gmail.com>
2017-09-02 11:45:54 +0530

    Initial Commit

---
+++ Connekt/app/src/main/java/com/hash/android/connekt/FirstLoginActivtity.java
@@ -0,0 +1,13 @@
+package com.hash.android.connekt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstLoginActivtity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_login_activtity);
+    }
+}
