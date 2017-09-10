45a460864b1bfbc0207ee6bf1deadb555015f047
sapt3 <saptarshi.ironmaiden@gmail.com>
2017-09-10 13:06:50 +0530

    Git test

--- TheJUApp/app/src/main/java/com/hash/android/thejuapp/DashboardActivity.java
+++ TheJUApp/app/src/main/java/com/hash/android/thejuapp/DashboardActivity.java
@@ -99,6 +99,7 @@ public class DashboardActivity extends AppCompatActivity
         txtName.setText(user.getName());
         emailTextView.setText(user.getEmail());
 
+        //Git test
         Glide.with(this)
                 .load(R.drawable.nav)
                 .crossFade()
